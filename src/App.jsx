e9d190386d23d81e63c3cd1e68840f12a649958f
KSHITIJ BAN <kshitjban2@gmail.com>
2025-11-19 21:11:11 +0545

    First Push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="First">
+      {" "}
+      <Firstcomponent />
+    </div>
+  )
+}
+function Firstcomponent() {
+  return <p>Frist Push in Git-Hub</p>
+}
+
+export default App
